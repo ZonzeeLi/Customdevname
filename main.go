47ac090f763b2a6c6caae1574fe59c8d621381ba
ZonzeeLi <61933511+ZonzeeLi@users.noreply.github.com>
2021-02-05 16:45:40 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,8 +9,9 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"strings"
+	"gopkg.in/gcfg.v1"
+	"time"
 )
 
 // 接收去GET到的json
@@ -37,34 +38,27 @@ type ResponseDev struct {
 	OriQuery string `json:"oriQuery"`
 	OriPy    string `json:"oriPy"`
 	Query    string `json:"query,omitempty"`
-	Devs     []struct{
-		Mac string `json:"mac"`
-		SubMac string `json:"subMac"`
-		Key string `json:"key"`
-		Mid string `json:"mid"`
-		DevName string `json:"devName"`
-		RoomName string `json:"roomName"`
-	} `json:"devs,omitempty"`
-	TarDevs []struct {
-		Mac string `json:"mac"`
-		SubMac string `json:"subMac"`
-		Mid string `json:"mid"`
-		Key string `json:"key"`
-		DevName string `json:"devName"`
-		RoomName string `json:"roomName"`
-		MidDes string `json:"midDes"`
-		DevNamePy string `json:"devNamePy"`
-	} `json:"tarDevs,omitempty"`
+	Devs     []TarDev `json:"devs,omitempty"`
+		//Mac string `json:"mac"`
+		//SubMac string `json:"subMac"`
+		//Key string `json:"key"`
+		//Mid string `json:"mid"`
+		//DevName string `json:"devName"`
+		//RoomName string `json:"roomName"`
+		//MidDes string `json:"midDes"`
+		//MidSe string `json:"midSe"`
+		//DevNamePy string `json:"devNamePy"`
+	TarDevs []TarDev `json:"tarDevs,omitempty"`
+		//Mac string `json:"mac"`
+		//SubMac string `json:"subMac"`
+		//Mid string `json:"mid"`
+		//Key string `json:"key"`
+		//DevName string `json:"devName"`
+		//RoomName string `json:"roomName"`
+		//MidDes string `json:"midDes"`
+		//MidSe string `json:"midSe"`
+		//DevNamePy string `json:"devNamePy"`
 }
-// TarDevs的接收结构体，从数据库中找匹配值，再添加其他值
-//type TarDev struct {
-//	Mac string `json:"mac"`
-//	SubMac string `json:"subMac"`
-//	Key string `json:"key"`
-//	Mid string `json:"mid"`
-//	DevName string `json:"devName"`
-//	RoomName string `json:"roomName"`
-//}
 
 type TarDev struct {
 	Mac string `json:"mac"`
@@ -74,21 +68,35 @@ type TarDev struct {
 	DevName string `json:"devName"`
 	RoomName string `json:"roomName"`
 	MidDes string `json:"midDes"`
+	MidSe string `json:"midSe"`
 	DevNamePy string `json:"devNamePy"`
 }
 // 数据库中模型绑定
 type mid_user struct {
 	Mid string `json:"mid"`
 	Definedname string `json:"definedname"`
+	Midse string `json:"midSe"`
 }
 
 var (
 	DB *gorm.DB
 )
 
-func InitMySQL()(err error){
+type Config struct {
+	Custom struct{
+		Username string
+		Password string
+		Sqlhost string
+		Sqlport string
+		Dbname string
+		Geturl string
+		Port string
+	}
+}
+
+func InitMySQL(dsn string)(err error){
 	// 绑定数据库 (本人数据库没有密码，库名为mid_dev)
-	dsn := ""
+
 	DB, err = gorm.Open("mysql",dsn)
 	if err != nil{
 		return err
@@ -112,8 +120,18 @@ func Py(s string) string {
 }
 
 func main() {
-	// 连接、绑定数据库（本人数据库没有密码，库名mid_dev）
-	err := InitMySQL()
+	// 加载配置文件
+	var config Config
+	err := gcfg.ReadFileInto(&config, "config.ini")
+	if err != nil {
+		fmt.Println("Failed to parse config file: %s", err)
+	}
+	dsn := config.Custom.Username + ":" + config.Custom.Password + "@tcp(" + config.Custom.Sqlhost + ":" + config.Custom.Sqlport + ")/" + config.Custom.Dbname + "?charset=utf8mb4&parseTime=True&loc=Local"
+	port := ":" + config.Custom.Port
+
+	// 连接、绑定数据库（库名mid_dev）
+
+	err = InitMySQL(dsn)
 	if err != nil {
 		panic(err)
 	}
@@ -135,8 +153,9 @@ func main() {
 		var localreturn ResponseDev
 		// devrep是通过pickup函数查询mac后返回回来的body
 
-		params := url.Values{}
-		Url, err := url.Parse("")
+		client := &http.Client{Timeout: time.Second}
+
+		req, err := http.NewRequest("GET",config.Custom.Geturl,nil)
 		if err != nil {
 			var nomac ResponseDev
 			nomac.Code = 1501
@@ -146,26 +165,56 @@ func main() {
 			c.JSON(http.StatusOK,nomac)
 			return
 		}
-		params.Set("mac",u.Mac)
-		Url.RawQuery = params.Encode()
-		urlPath := Url.String()
-		//fmt.Println(urlPath)
+		q := req.URL.Query()
 
-		//client := http.Client{Timeout: time.Second}
+		q.Set("mac",u.Mac)
+		req.URL.RawQuery = q.Encode()
 
-		resp,err := http.Get(urlPath)
-		if err != nil {
+		resp, er := client.Do(req)
+		if er != nil {
 			var nomac ResponseDev
 			nomac.Code = 1501
 			nomac.OriQuery = u.Query
 			nomac.OriPy = Py(u.Query)
-			fmt.Println(err)
+			fmt.Println(er)
 			c.JSON(http.StatusOK,nomac)
 			return
 		}
 		defer resp.Body.Close()
 		body, _ := ioutil.ReadAll(resp.Body)
-		//fmt.Println(string(body))
+
+
+		//params := url.Values{}
+		//Url, err := url.Parse("")
+		//if err != nil {
+		//	var nomac ResponseDev
+		//	nomac.Code = 1501
+		//	nomac.OriQuery = u.Query
+		//	nomac.OriPy = Py(u.Query)
+		//	fmt.Println(err)
+		//	c.JSON(http.StatusOK,nomac)
+		//	return
+		//}
+		//params.Set("mac",u.Mac)
+		//Url.RawQuery = params.Encode()
+		//urlPath := Url.String()
+		////fmt.Println(urlPath)
+		//
+		////client := http.Client{Timeout: time.Second}
+		//
+		//resp,err := http.Get(urlPath)
+		//if err != nil {
+		//	var nomac ResponseDev
+		//	nomac.Code = 1501
+		//	nomac.OriQuery = u.Query
+		//	nomac.OriPy = Py(u.Query)
+		//	fmt.Println(err)
+		//	c.JSON(http.StatusOK,nomac)
+		//	return
+		//}
+		//defer resp.Body.Close()
+		//body, _ := ioutil.ReadAll(resp.Body)
+		////fmt.Println(string(body))
 		var devrep Devroom
 		err = json.Unmarshal(body,&devrep)
 
@@ -178,8 +227,7 @@ func main() {
 			nodev.Code = 1401
 			nodev.OriQuery = u.Query
 			nodev.OriPy = Py(u.Query)
-			//nodev.TarDevs = nil
-			nodev.Devs = devrep.Devs
+			//nodev.Devs = devrep.Devs
 			c.JSON(http.StatusOK,nodev)
 		}else {
 			var mark []int
@@ -200,15 +248,35 @@ func main() {
 				localreturn.OriQuery = u.Query
 				localreturn.OriPy = Py(u.Query)
 				//localreturn.TarDevs = nil
-				localreturn.Devs = devrep.Devs
+				for i := 0;i < n;i ++ {
+					var sample TarDev
+					sample.Mid = devrep.Devs[i].Mid
+					sample.Mac = devrep.Devs[i].Mac
+					sample.DevName = devrep.Devs[i].DevName
+					sample.DevNamePy = Py(devrep.Devs[i].DevName)
+					sample.Key = devrep.Devs[i].Key
+					sample.SubMac = devrep.Devs[i].SubMac
+					sample.RoomName = devrep.Devs[i].RoomName
+
+					var sqlfind mid_user
+					DB.Table("mid_user").Select("definedname,midse").Where("mid = ?",devrep.Devs[i].Mid).First(&sqlfind)
+					sample.MidSe = sqlfind.Midse
+					sample.MidDes = sqlfind.Definedname
+
+					localreturn.Devs = append(localreturn.Devs,sample)
+
+				}
+				//localreturn.Devs = devrep.Devs
 				c.JSON(http.StatusOK,localreturn)
 			}else if len(mark) == 1 {
 				// 通过mid去数据库中查询，然后把相应的name返回赋值
 				localreturn.Code = 0
 				localreturn.OriQuery = u.Query
 				localreturn.OriPy = Py(u.Query)
+
+
 				var sqlfind mid_user
-				DB.Table("mid_user").Select("definedname").Where("mid = ?",devrep.Devs[mark[0]].Mid).First(&sqlfind)
+				DB.Table("mid_user").Select("definedname, midse").Where("mid = ?",devrep.Devs[mark[0]].Mid).First(&sqlfind)
 
 				var box TarDev
 				box.Mid = devrep.Devs[mark[0]].Mid
@@ -219,7 +287,9 @@ func main() {
 				box.RoomName = devrep.Devs[mark[0]].RoomName
 				box.MidDes = sqlfind.Definedname
 				box.DevNamePy = Py(devrep.Devs[mark[0]].DevName)
+				box.MidSe = sqlfind.Midse
 
+				//fmt.Println(sqlfind.Definedname,sqlfind.MidSe)
 				var replace string
 				replace = strings.ReplaceAll(u.Query, box.DevName, box.MidDes)
 				localreturn.Query = replace
@@ -231,21 +301,23 @@ func main() {
 				localreturn.Code = 0
 				localreturn.OriQuery = u.Query
 				localreturn.OriPy = Py(u.Query)
+				localreturn.Query = u.Query
 				//localreturn.Devs = nil
 				//localreturn.TarDevs = make([]TarDev, 0, len(mark))
 				for i := 0;i < len(mark);i ++ {
 					var sqlfind mid_user
-					DB.Table("mid_user").Select("definedname").Where("mid = ?",devrep.Devs[mark[i]].Mid).First(&sqlfind)
+					DB.Table("mid_user").Select("definedname,midse").Where("mid = ?",devrep.Devs[mark[i]].Mid).First(&sqlfind)
 					//fmt.Println(sqlfind)
 					var box TarDev
 					box.Mid = devrep.Devs[mark[i]].Mid
-					box.SubMac = ""
+					box.SubMac = devrep.Devs[mark[i]].SubMac
 					box.Mac = devrep.Devs[mark[i]].Mac
 					box.Key = devrep.Devs[mark[i]].Key
 					box.DevName = devrep.Devs[mark[i]].DevName
 					box.RoomName = devrep.Devs[mark[i]].RoomName
 					box.MidDes = sqlfind.Definedname
 					box.DevNamePy = Py(devrep.Devs[mark[i]].DevName)
+					box.MidSe = sqlfind.Midse
 
 					localreturn.TarDevs = append(localreturn.TarDevs,box)
 				}
@@ -253,5 +325,5 @@ func main() {
 			}
 		}
 	})
-	r.Run(":9090")
+	r.Run(port)
 }
